7849afca197264f94bbbef9f8b2a453d90cfe6b3
briand07 <briandiaz3720@gmail.com>
2023-08-22 16:58:50 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -5,5 +5,9 @@ namespace TrabajoSube;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
-   
+    public function testVerdadero()
+    {
+        // Aquí, simplemente estamos usando una aserción que siempre será verdadera.
+        $this->assertTrue(true);
+    }
 }
