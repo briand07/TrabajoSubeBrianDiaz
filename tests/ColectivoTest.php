b4f8877b1a7f13e3c9e96ad327ac97e8c6e89706
JuliamCabrera <julianmateocabrera@gmail.com>
2023-09-25 22:12:25 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -3,19 +3,7 @@
 namespace TrabajoSube;
 
 use PHPUnit\Framework\TestCase;
-
-class Colectivo{
-    protected $linea;
-    
-    public function __construct($linea){
-        $this->linea = $linea;
-    }
-    
-    //    Funcion de ejemplo para test
-    public function getLinea(){
-        return $this->linea;
-    }
-}
+use TrabajoSube\Colectivo;
 
 class ColectivoTest extends TestCase{
 
