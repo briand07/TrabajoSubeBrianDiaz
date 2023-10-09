188e04c7fb0ce70b96dd98cde0302812bcc2dbc8
JuliamCabrera <julianmateocabrera@gmail.com>
2023-10-09 11:30:13 -0300

    Iteración 1 Completada

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -8,23 +8,3 @@ use TrabajoSube\comida;
 use TrabajoSube\Boleto;
 
 class ColectivoTest extends TestCase{}
-
-    $tarjeta = new Tarjeta(500); // Cargar una tarjeta con $500 de saldo
-echo "Saldo inicial en la tarjeta: $" . $tarjeta->getSaldo() . PHP_EOL;
-
-$tarjeta->cargarSaldo(300); // Cargar $300 de saldo
-$tarjeta->cargarSaldo(1000); // Cargar $1000 de saldo
-$tarjeta->cargarSaldo(2000); // Cargar $2000 de saldo
-$tarjeta->cargarSaldo(4000); // Intentar cargar $5000 de saldo (supera el límite)
-
-echo "Saldo actual en la tarjeta: $" . $tarjeta->getSaldo() . PHP_EOL;
-
-$colectivo = new Colectivo();
-
-$boleto = $colectivo->pagarCon($tarjeta);
-if ($boleto !== null) {
-    echo "Se ha realizado el pago de $" . $boleto->getMonto() . " correctamente." . PHP_EOL;
-    echo "Saldo restante en la tarjeta: $" . $tarjeta->getSaldo() . PHP_EOL;
-} else {
-    echo "No hay suficiente saldo en la tarjeta para pagar el pasaje." . PHP_EOL;
-}
