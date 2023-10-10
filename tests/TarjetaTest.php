179a00dace84599bf0ebbfcf4cc38e4f5a103656
briand07 <briandiaz3720@gmail.com>
2023-10-10 16:17:33 -0300

    tests tarjeta

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,7 +10,7 @@ use TrabajoSube\Boleto;
 class TarjetaTest extends TestCase {
 
     public function testPagarConSaldoSuficiente() {
-        $tarjeta = new Tarjeta(150); // Saldo suficiente
+        $tarjeta = new Tarjeta(150);
         $colectivo = new Colectivo();
 
         $boleto = $colectivo->pagarCon($tarjeta);
@@ -24,6 +24,39 @@ class TarjetaTest extends TestCase {
         $boleto = $colectivo->pagarCon($tarjeta);
         $this->assertEquals(-210, $tarjeta->getSaldo());
         $this->assertEquals(False, $colectivo->pagarCon($tarjeta));
+        
     }
 
+    public function testSaldoMenor() {
+        $tarjeta = new Tarjeta(150); 
+        $colectivo = new Colectivo();
+
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+
+        $this->assertLessThan($tarjeta->getSaldo(), -211.84);
+    }
+
+    public function testDescuentoPlus() {
+        $tarjeta = new Tarjeta(150); 
+        $colectivo = new Colectivo();
+
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertEquals(-90, $tarjeta->getSaldo());
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertEquals(-210, $tarjeta->getSaldo());
+    }
+
+    public function testFanquiciaCompleta() {
+        $tarjeta = new FranquiciaCompleta(150); 
+        $colectivo = new Colectivo();
+
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertEquals(-90, $tarjeta->getSaldo());
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertEquals(-210, $tarjeta->getSaldo());
+    }
 }
