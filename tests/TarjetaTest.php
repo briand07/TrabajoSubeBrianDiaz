08e2222510dbd4c36822ce4a5d44dbb8ec4b4968
briand07 <briandiaz3720@gmail.com>
2023-10-10 15:26:32 -0300

    Clases Iteración 2

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,23 +10,20 @@ use TrabajoSube\Boleto;
 class TarjetaTest extends TestCase {
 
     public function testPagarConSaldoSuficiente() {
-        $tarjeta = new Tarjeta(200); // Saldo suficiente
+        $tarjeta = new Tarjeta(150); // Saldo suficiente
         $colectivo = new Colectivo();
 
         $boleto = $colectivo->pagarCon($tarjeta);
-
-        $this->assertInstanceOf(Boleto::class, $boleto);
+        
         $this->assertEquals(120, $boleto->getMonto());
-        $this->assertEquals(80, $tarjeta->getSaldo());
-    }
-
-    public function testPagarSinSaldoSuficiente() {
-        $tarjeta = new Tarjeta(50); // Saldo insuficiente
-        $colectivo = new Colectivo();
-
+        $this->assertEquals(30, $tarjeta->getSaldo());
+        
         $boleto = $colectivo->pagarCon($tarjeta);
-
-        $this->assertFalse($boleto);
-        $this->assertEquals(50, $tarjeta->getSaldo());
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertEquals(-210, $tarjeta->getSaldo());
+        $this->assertEquals(False, $colectivo->pagarCon($tarjeta));
     }
+
 }
